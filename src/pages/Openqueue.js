0e62b5afdf0baf8b66c73b812187dd09925e2f1a
flyingPie23 <drissfreah325@gmail.com>
2025-04-24 11:08:58 +0100

    data entry

--- src/pages/Openqueue.js
+++ src/pages/Openqueue.js
@@ -44,14 +44,14 @@ function Openqueue(props) {
 
         <div className='flex justify-center my-[18px]'>
           <a href={props.char[hero].link} target="_blank">
-            <h1 className='text-[55px] font-bold text-center text-white'>{props.char[hero].name}</h1>
+            <h1 className='text-[55px] font-bold text-center text-white hover:underline'>{props.char[hero].name}</h1>
           </a>
         </div>
 
         <div className='flex m-auto place-content-around'>
           <Card1 icon={props.char[hero].challanges[0].icon} imgClass="h-[55px] w-[55px]" text={props.char[hero].challanges[0].text} textClass={"text-[16px] text-center text-white mx-3 px-3 text-end"} cardClass="flex border-[5px] border-orange rounded-xl w-[320px] p-3"/>
 
-          <a onClick={handleClick} href="#" class="bg-orange hover:bg-orange-dark p-2 rounded-md border-[2px] border-white w-[100px] text-center text-white h-[55px] "> Re-Roll</a>
+          <a onClick={handleClick} href="#" class="bg-orange hover:bg-orange-dark p-2 my-[24px] rounded-md border-[2px] border-white w-[100px] text-center text-white h-[55px] "> Re-Roll</a>
 
           <Card2 icon={props.char[hero].challanges[1].icon} imgClass="h-[55px] w-[55px]" text={props.char[hero].challanges[1].text} textClass={"text-[16px] text-center text-white mx-3 px-3 text-end"} cardClass="flex border-[5px] border-orange rounded-xl w-[320px] p-3"/>
         </div>
