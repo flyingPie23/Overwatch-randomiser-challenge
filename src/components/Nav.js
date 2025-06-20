b68eaec48725403ff66d81744dbbd0c8994af5dd
driss <freah>
2025-06-20 22:24:36 +0100

    saving for deploying

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ import {Img, Btn} from "./components"
 function Nav() {
   return (
     <div className='p-3 m-3 mx-[55px] flex place-content-between'>
-      <Btn href="/patchnotes" name="Patch notes"class="font-bold text-white bg-orange hover:bg-orange-dark p-1 rounded-md"/>
-      <a href='/'>
+      <Btn href="/#/patchnotes" name="Patch notes"class="font-bold text-white bg-orange hover:bg-orange-dark p-1 rounded-md"/>
+      <a href='/#/'>
         <Img src="https://www.pngall.com/wp-content/uploads/13/Overwatch-Logo-PNG.png" class="h-6"/>
       </a>
       <Btn href="https://overwatch.blizzard.com/en-us/" target="_blank" name="Get Overwatch"class="font-bold text-white bg-orange hover:bg-orange-dark p-1 rounded-md"/>
